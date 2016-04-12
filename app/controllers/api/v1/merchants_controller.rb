bb99fb6f0dec811adc2452f5fb1b1d95c126386c
Chelsea Johnson <cheljoh@gmail.com>
2016-04-11 20:52:29 -0600

    Find
    Working on find capability. Planning on some migrations to do case insensitive searches.

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -2,12 +2,19 @@ module Api
   module V1
     class MerchantsController < ApiController
       respond_to :json
+
       def index
         respond_with Merchant.all
       end
 
       def show
-        respond_with Merchant.find(params[:id])
+        respond_with Merchant.find_by(merchant_params)
+      end
+
+      private
+
+      def merchant_params
+        params.permit(:id, :name, :created_at, :updated_at)
       end
     end
   end
--- spec/requests/merchants_spec.rb
+++ spec/requests/merchants_spec.rb
@@ -6,4 +6,16 @@ RSpec.describe "Merchants Spec", :type => :request do
     get "/api/v1/merchants.json"
     expect(response.content_type).to eq("application/json")
   end
+
+  it "returns a single merchant" do
+    merchant = Merchant.create(name: "Cool Things")
+    get "/api/v1/merchants/#{merchant.id}.json"
+    expect(response.content_type).to eq("application/json")
+  end
+
+  it "can find a merchant" do
+    merchant = Merchant.create(name: "Cool Things")
+    get "/api/v1/merchants/find?id=#{merchant.id}"
+    expect(response.content_type).to eq("application/json")
+  end
 end
