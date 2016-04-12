c0a419168c89114f611de6ee946cd6ff25308bb8
Chelsea Johnson <cheljoh@gmail.com>
2016-04-12 16:54:10 -0600

    Format unit price
    Formatted unit price in rake tasks, all spec harness tests are passing.

--- spec/requests/invoice_items_spec.rb
+++ spec/requests/invoice_items_spec.rb
@@ -24,16 +24,11 @@ RSpec.describe "Invoice Items Spec", :type => :request do
 
     expect(response.content_type).to eq("application/json")
     expect(response).to be_success
-    expect(invoice_item_parsed["unit_price"]).to eq("12532.0")
+    expect(invoice_item_parsed["unit_price"]).to eq("125.32")
     expect(invoice_item_parsed["id"]).to eq(invoice_item.id)
   end
 
   it "returns an invoice item through find" do
-    # customer = Customer.create(first_name: "Hello", last_name: "Julia")
-    # merchant = Merchant.create(name: "Cool Things")
-    # invoice = Invoice.create(customer_id: customer.id, merchant_id: merchant.id, status: "shipped")
-    # item = Item.create(name: "tool", description: "really neat", unit_price: 12345)
-    # invoice_item = InvoiceItem.create(item_id: item.id, invoice_id: invoice.id, unit_price: 12345)
     make_invoice_items
     invoice_item = InvoiceItem.last
     customer = Customer.last
@@ -44,7 +39,7 @@ RSpec.describe "Invoice Items Spec", :type => :request do
 
     expect(response).to be_success
     expect(response.content_type).to eq("application/json")
-    expect(invoice_item_parsed["unit_price"]).to eq("12532.0")
+    expect(invoice_item_parsed["unit_price"]).to eq("125.32")
     expect(invoice_item_parsed["id"]).to eq(invoice_item.id)
   end
 
